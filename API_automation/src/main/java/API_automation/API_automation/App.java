f62a3fa2fa6d28abcc53c05205469f6b6bca2042
div1997 <diya123verma@gmail.com>
2020-02-05 15:28:37 +0530

    API automation maven

---
+++ API_automation/src/main/java/API_automation/API_automation/App.java
@@ -0,0 +1,13 @@
+package API_automation.API_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
